b12f7cf0632d35e45656e01b293ee12c745d9ddf
mokshada <mokshadabhandare.01@gmail.com>
2024-03-19 10:54:29 +0530

    sql

--- first.session.sql
+++ first.session.sql
@@ -1,11 +1,1 @@
-use first;
-ALTER TABLE student add column email varchar(50);
-
-UPDATE student set email="moksh@gmail.com" where id=1;
-UPDATE student set email="manish@gmail.com" where id=2;
-
-INSERT into student (id,name,marks,age,email)values
-                     (3,"Archana",85,22,"archana@gmail.com");
-
-
-SELECT * from student where marks>80;
+SELECT * from student WHERE name="Moksh";
