bd18dad6925f36658ac5a2d16c5916f5d5074293
mokshada <mokshadabhandare.01@gmail.com>
2024-03-19 11:33:12 +0530

    sql

--- sql-1.sql
+++ sql-1.sql
@@ -1,4 +1,13 @@
+create database first;
 use first;
+
+CREATE table student (id int primary key,name varchar(40),marks int,age int);
+
+
+INSERT into student (id,name,marks,age)values
+                     (1,"Moksh",80,22),
+                     (2,"Manish",95,23);
+                     
 ALTER TABLE student add column email varchar(50);
 
 UPDATE student set email="moksh@gmail.com" where id=1;
@@ -13,3 +22,5 @@ SELECT * from student where marks>80;
 SELECT * from student WHERE name="Moksh";
 
 ALTER table student drop column email;
+
+delete from student where  marks>80;
