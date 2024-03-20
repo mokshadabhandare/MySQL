c2bbded277f53ef179e55e827296a2098e590589
mokshada <mokshadabhandare.01@gmail.com>
2024-03-20 12:00:11 +0530

    sql

--- sql-2.sql
+++ sql-2.sql
@@ -9,10 +9,17 @@ INSERT into student(id,name,class,batch)values
                    (1,"Akash","B.tech",2023),
                    (2,"Anjali","BE",2021),
                    (3,"Ciya","MCA",2024);
-                   
+
 
 INSERT into teacher(id,name,subject,department,student_id)values
                    (1,"Joshi Sir","Computer Science","Dept of CS",3),
                    (2,"Kulkarni Mam","Maths","Dept of Math",1),
                    (3,"Rupali Mam","Java","Dept of CS",3),
                    (4,"Kiran Mam","DBMS","Dept of IT",1);
+
+
+select teacher.id, teacher.name , teacher.subject , teacher.department from teacher
+join student on teacher.student_id = student.id where student.class="MCA";
+
+select teacher.id, teacher.name , teacher.subject , teacher.department from teacher
+join student on teacher.student_id = student.id where student.class="B.tech";
