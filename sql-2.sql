390f08988c6c7880b49f765d3a501822c823e837
mokshada <mokshadabhandare.01@gmail.com>
2024-03-20 11:19:52 +0530

    sql

---
+++ sql-2.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE college;
+use college;
+
+CREATE TABLE student(id int primary key,name varchar(40),class varchar(50),batch int);
+
+CREATE TABLE teacher(id int primary key,name varchar(40),subject varchar(50),department varchar(40),student_id int,foreign key(student_id) references student(id));
+
+INSERT into student(id,name,class,batch)values
+                   (1,"Akash","B.tech",2023),
+                   (2,"Anjali","BE",2021),
+                   (3,"Ciya","MCA",2024);
+                   
+
+INSERT into teacher(id,name,subject,department,student_id)values
+                   (1,"Joshi Sir","Computer Science","Dept of CS",3),
+                   (2,"Kulkarni Mam","Maths","Dept of Math",1),
+                   (3,"Rupali Mam","Java","Dept of CS",3),
+                   (4,"Kiran Mam","DBMS","Dept of IT",1);
